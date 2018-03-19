1e5af1514ffc2f743dae560fa4f2c27a8f1841c8
VARADHARAJAN <svaradharajan768@gmail.com>
2018-03-19 21:14:03 +0530

    Commit first program to print url from a string

--- NamePrinter.py
+++ NamePrinter.py
@@ -1,2 +1,9 @@
 name = "Varadharajan"
-print "Hello "+ name
+print "Hello "+ name +'!'*3
+print name[0]
+print name[0:7]
+print name[8:]
+print name[:9]
+print name[:]
+print name[:-1]
+print name[:35]
